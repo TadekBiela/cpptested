4fdb5d8c273f042775873743bff3236b2495906c
TadekBiela <TadekBiela@users.noreply.github.com>
2025-07-07 08:14:56 +0000

    deploy: eccdcfc5b9b57e07c8f7d3d0bcaa830d3150cb53

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,1 @@
-var store = [{
-        "title": "Testowy post dodany do repozytorium 4go lipca",
-        "excerpt":"To tylko test czy post mimo dodania go dzień wcześniej do repozytorium, pojawi się dopiero 5go Lipca 2025. Mam nadzieję, że to zadziała :)   Autor: Tadeusz Biela  Programista C++ | Entuzjasta TDD | Fan unit testów   LinkedIn  ","categories": [],
-        "tags": [],
-        "url": "/test-post/",
-        "teaser": null
-      }]
+var store = []
