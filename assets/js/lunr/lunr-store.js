68aa36f676d1603a67959ad813ef5f02432667e2
TadekBiela <TadekBiela@users.noreply.github.com>
2025-07-28 06:49:40 +0000

    deploy: f9483002688a9b3fa23e92d8fb88e9143429b231

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "F.I.R.S.T. - jak pisać unit testy lepiej.",
+        "excerpt":"F.I.R.S.T. to pewien standard, jaki unit testy powinny spełniać. To zbiór założeń, które wyznaczają kierunek, jaki powinniśmy obierać, pisząc testy. F jak Fast Czas oczekiwania na wynik naszych unit testów powinien być jak najkrótszy. Znani i cenieni specjaliści, jak Robert C. Martin czy Kent Beck, w swoich książkach przytaczają związek między czasem wykonywania testów,...","categories": ["unit testing"],
+        "tags": ["clean code","software testing","code quality","refactoring","developer practices"],
+        "url": "/unit%20testing/first-reguly-ut/",
+        "teaser": null
+      }]
