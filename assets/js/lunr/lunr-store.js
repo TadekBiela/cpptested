1ed51fe71e2f3d2e6d8263dfa030fa3aa9b34d90
TadekBiela <TadekBiela@users.noreply.github.com>
2026-02-25 22:43:21 +0000

    deploy: 88ca381bb2e5b4ea23fe3d947350623d1ea01708

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -17,7 +17,7 @@ var store = [{
         "url": "/unit-testing/AAA-golden-standard/",
         "teaser": null
       },{
-        "title": "Wątki i&nbsp;wyjątki. Jak radzić sobie z&nbsp;nieoczekiwanymi zachowaniami w&nbsp;wielowątkowym kodzie.",
+        "title": "Wątki i wyjątki. Jak radzić sobie z&nbsp;nieoczekiwanymi zachowaniami w&nbsp;wielowątkowym kodzie.",
         "excerpt":"Nieoczekiwane zachowanie czyli wyjątek to sytuacja, w której nasz kod zachował się w sposób, który normalnie nie występuje. Może być to wywołane problemami z zasobami jak brak pamięci podręcznej lub brak dostępu do pliku. Powodów może być wiele, najczęściej nie zależą od nas. Jednak możemy się na takie sytuacje przygotować i pomimo wystąpienia wyjątków, nasz program nie...","categories": ["multithreading"],
         "tags": ["cpp","exceptions","multithreading","software"],
         "url": "/multithreading/exception-in-thread/",
